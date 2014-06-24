ae7eaedb1042728b40eca481155e37a5a4a95b9e
Raheel Ahmad <ahmadraheel@gmail.com>
2014-06-23 22:19:30 -0700

    Chapter 2: add contentsRect

--- CABook.playground/section-1.swift
+++ CABook.playground/section-1.swift
@@ -11,9 +11,40 @@ import QuartzCore
 CALayer.contents can be AnyObject, but is drawn only for a CGImage object (not UIImage)
 */
 
-Chapter2.masksToBounds()
+//Chapter2.contentsRectSprites()
 
 struct Chapter2 {
+	
+	static func contentsRectSprites() {
+		let spriteImage = UIImage(named: "monuments_sprite.png")
+		let jantar = CGRect(x: 0, y: 0, width: 377/800.0, height: 245/800.0)
+		let nalanda = CGRect(x: 413/800.0, y: 0, width: 384/800.0, height: 245/800.0)
+		let fort = CGRect(x: 0.0, y: 260/800.0, width: 423/800.0, height: 280/800.0)
+		let spriteRects = [ jantar, nalanda, fort ]
+		
+		for rect in spriteRects {
+			
+			var baseView = Helpers.basicView(size: CGSize(width: rect.size.width * 800.0, height: rect.size.height * 800.0))
+			baseView.layer.contents = spriteImage.CGImage
+			baseView.layer.contentsGravity = kCAGravityResizeAspect
+			baseView.layer.contentsRect = rect
+			baseView // timeline this
+		}
+	}
+	
+	static func contentsRect() {
+		let image = Helpers.tajImage()
+		var baseView = Helpers.basicView(size: image.size)
+		baseView.layer.contents = image.CGImage
+		// can specify contentsRect in *relative coordinates*
+		baseView.layer.contentsRect = CGRect(x: 0, y: 0, width: 0.4, height: 0.4)
+		// unit can be > 0 or < 0
+		baseView.layer.contentsRect = CGRect(x: 0.2, y: 0.2, width: 1.4, height: 1.4)
+		baseView.layer.contentsRect = CGRect(x: -0.2, y: -0.2, width: 1.4, height: 1.4)
+		
+		baseView // timeline this
+	}
+	
 	static func masksToBounds() {
 		var baseView = Helpers.basicView(size: CGSize(width: 800, height: 800))
 		
@@ -26,12 +57,15 @@ struct Chapter2 {
 		tajLayer.frame = CGRect(x: 0, y: 0, width: image.size.width, height: image.size.height)
 		tajLayer.contents = image.CGImage
 		
+		// Adding the image layer to the container layer
 		containerLayer.addSublayer(tajLayer)
 		baseView.layer.addSublayer(containerLayer)
 		
+		// since containerLayer's `masksToBounds` is false by default, tajLayer will overflow its superLayer's bounds
+		// set the property to true to clip the image
 //		containerLayer.masksToBounds = true
 		
-		baseView
+		baseView // timeline this
 	}
 	
 	static func layerContentsGravity() {
@@ -48,7 +82,7 @@ struct Chapter2 {
 		var view = Helpers.basicView()
 		// Apparently, with Swift's bridging, we don't have to explcitly cast with (__bridge id)
 		view.layer.contents = tajImage.CGImage
-		view
+		view // timeline this
 	}
 	
 }
@@ -78,7 +112,7 @@ struct Chapter1 {
 		hostedLayer.frame = CGRect(x: 20, y: 20, width: 260, height: 360)
 		hostedLayer.backgroundColor = UIColor(hue: 0.3, saturation: 1.0, brightness: 0.8, alpha: 1.0).CGColor
 		view.layer.addSublayer(hostedLayer)
-		view
+		view // timeline this
 	}
 }
 
